db6a4d087938eb0fd9b259709491f2b45d52b6a4
Razhel <mateuszszpakowski@wp.pl>
2020-03-18 19:51:14 +0100

    Movie model added

--- movie_database/models.py
+++ movie_database/models.py
@@ -1,1 +1,11 @@
+import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    year = models.PositiveIntegerField(validators=[MinValueValidator(1900), max_value_current_year])
+    type = models.CharField(max_length=128, unique=True)
+    poster = models.TextField()
+    user = models.ManyToManyField(User)
